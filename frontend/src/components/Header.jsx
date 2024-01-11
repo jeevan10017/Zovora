063dd02895025e15819771bae947e7b8c8ed9f5a
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2024-01-11 12:34:55 +0530

    Home screen

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from '../assets/zovoralogoo.png'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="black" variant="dark" expand="md" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="zovora"/>
+         </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/cart">
+              <FaShoppingCart />
+              Cart
+            </Nav.Link>
+             <Nav.Link href="/login">
+            <FaUser/>
+            Sign in
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
